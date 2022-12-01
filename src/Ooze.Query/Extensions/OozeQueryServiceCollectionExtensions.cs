5e212b980a320ff69e66cd43a1002605a612c319
Denis Pavlović <denis.pav@hotmail.com>
2022-12-01 22:25:53 +0100

    Add OozeQueryBuilder

--- src/Ooze.Query/Extensions/OozeQueryServiceCollectionExtensions.cs
+++ src/Ooze.Query/Extensions/OozeQueryServiceCollectionExtensions.cs
@@ -1,34 +1,9 @@
-﻿using Ooze.Query.Filters;
+﻿using Microsoft.Extensions.DependencyInjection;
 
 namespace Ooze.Query.Extensions;
 
 public static class OozeQueryServiceCollectionExtensions
 {
-    private static readonly Type QueryFilterProviderType = typeof(IOozeQueryFilterProvider<>);
-
-    //TODO fix
-    // public static IOozeServiceCollectionBuilder AddQueryHandler(this IOozeServiceCollectionBuilder oozeBuilder)
-    // {
-    //     oozeBuilder.Services.AddScoped(typeof(IOozeQueryHandler<>), typeof(OozeQueryHandler<>));
-    //     return oozeBuilder;
-    // }
-    //
-    // public static IOozeServiceCollectionBuilder AddQueryFilter<TQueryFilter>(
-    //     this IOozeServiceCollectionBuilder oozeBuilder,
-    //     ServiceLifetime providerLifetime = ServiceLifetime.Singleton)
-    // {
-    //     var queryFilterType = typeof(TQueryFilter);
-    //     var implementedInterfaces = queryFilterType.GetInterfaces()
-    //         .Where(@type => type.IsGenericType)
-    //         .ToList();
-    //     var queryFilterProvider = implementedInterfaces.SingleOrDefault(@interface =>
-    //         QueryFilterProviderType.IsAssignableFrom(@interface.GetGenericTypeDefinition()));
-    //
-    //     if (queryFilterProvider is not null)
-    //     {
-    //         oozeBuilder.Services.Add(new ServiceDescriptor(queryFilterProvider, queryFilterType, providerLifetime));
-    //     }
-    //
-    //     return oozeBuilder;
-    // }
+    public static IOozeQueryBuilder AddOozeQuery(this IServiceCollection services)
+        => new OozeQueryBuilder(services);
 }
